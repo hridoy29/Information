febf695ca1e5150cd94c3375db20dd986a0d9d3c
jubair hridoy <jubair_29@outlook.com>
2021-05-25 23:24:14 +0600

    Add project files.

---
+++ Information/Class/DataAccessLayer.cs
@@ -0,0 +1,35 @@
+﻿using GuestFeedBackForm.Class;
+using Information.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace Information.Class
+{
+    public class DataAccessLayer
+    {
+
+        DBExecutor dbExecutor;
+       
+        public DataAccessLayer()
+        {
+            dbExecutor = new DBExecutor();
+        }
+      
+        public List<Employee> EmployeeInfo(int Id)
+        {
+            List<Employee> _Employee_Info_Result = new List<Employee>();
+
+            Parameters[] colparameters = new Parameters[1]{
+                new Parameters("@_EmployeeId", Id, DbType.Int32, ParameterDirection.Input)
+            };
+            _Employee_Info_Result = dbExecutor.FetchData<Employee>(CommandType.StoredProcedure, "Employee_Info", colparameters);
+
+            return _Employee_Info_Result;
+        }
+        
+    }
+}
---
+++ Information/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Information.Class;
+using Information.Models;
+
+namespace Information.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DataAccessLayer dataAccessLayer = new DataAccessLayer();
+       
+        
+        [HttpGet]
+        public ActionResult Index(List<Employee> empDetails)
+        {
+            List<Employee> emp =  (List<Employee>)TempData["Employee"];
+            return View(emp);
+        }
+
+        public ActionResult EmployeeInfo(int Id)
+        {
+            using (BSEntities context = new BSEntities())
+            {
+                IEnumerable<Employee> empDetails = dataAccessLayer.EmployeeInfo(Id);
+                TempData["Employee"] = empDetails;
+                return RedirectToAction("Index", "Home", empDetails);
+            }
+           
+            
+        }
+    }
+}
+
+//    @*@foreach (var item in (List<Information.Models.Employee>)ViewData["Employee"])*@
